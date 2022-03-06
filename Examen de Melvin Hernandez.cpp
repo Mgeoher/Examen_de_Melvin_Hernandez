76b8364ff536b8ef975e344a0c91e5b1e5d03fae
mgeoher <mhernandezm85@miumg.edu.gt>
2022-03-05 19:23:25 -0600

    se añade el ejercicio 2

--- Examen de Melvin Hernandez.cpp
+++ Examen de Melvin Hernandez.cpp
@@ -8,11 +8,12 @@ long double factorial;
   					
 
    					
-int main(){
-	
+int main(int argc, char *argv[]){
+	//Ejercicio 2
+	int numero;
 	cout << "Bienvenido \n";
 	cout << "Que ejercicio desea realizar \n";
-	cout << "Ejercicio1 \n";
+	cout << "Ejercicio1 \nEjercicio2 \n";
 	cin >> opejercicio ;
 	switch (opejercicio){
 		case 1: 	  					
@@ -24,6 +25,17 @@ int main(){
    					cout << endl << "Factorial de " << n << " -> " << factorial << endl;
    					system("pause");
    					break;
+   		case 2: 
+		   			do{
+					cout<<"Ingrese un numero: ";
+					cin>>numero;
+		
+						}while((numero<1) || (numero>10));
+	
+						for(int i=1; i<=10; i++){
+							cout<<numero<<" * "<<i<<" = "<<numero * i<<endl;
+					} break;
+	return 0;			
 } 
 	
 return 0;	}
