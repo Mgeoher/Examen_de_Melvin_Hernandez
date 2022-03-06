33830b6d267f1b30f6420f925f21932a70b47fda
mgeoher <mhernandezm85@miumg.edu.gt>
2022-03-05 19:36:19 -0600

    se añade el ejercicio numero 3

--- Examen de Melvin Hernandez.cpp
+++ Examen de Melvin Hernandez.cpp
@@ -7,13 +7,14 @@ int n,i;
 long double factorial; 
   					
 
+
    					
 int main(int argc, char *argv[]){
 	//Ejercicio 2
 	int numero;
 	cout << "Bienvenido \n";
 	cout << "Que ejercicio desea realizar \n";
-	cout << "Ejercicio1 \nEjercicio2 \n";
+	cout << "Ejercicio1 \nEjercicio2 \nEjercicio3\n";
 	cin >> opejercicio ;
 	switch (opejercicio){
 		case 1: 	  					
@@ -35,7 +36,24 @@ int main(int argc, char *argv[]){
 						for(int i=1; i<=10; i++){
 							cout<<numero<<" * "<<i<<" = "<<numero * i<<endl;
 					} break;
-	return 0;			
+	return 0; // ejercicio 3
+		case 3: {int n, cp=0,ci=0,c=0,x;
+		cout<<"Ingresar cantidad de numeros:";
+		cin>>n;
+		while (c<n)
+		{
+		cout<<"Ingresar numero:";
+		cin>>x;
+		if(x % 2==0)
+		cp=cp+1;
+		else
+		ci=ci+1;
+		c=c+1;
+		}
+		cout<<"los numeros pares son:"<<cp<<endl;
+		cout<<"los numeros impares son:"<<ci<<endl;
+		
+		}			
 } 
 	
 return 0;	}
